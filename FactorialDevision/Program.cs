48afc3d263b746e7af6f291cfafb174834bfda3e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 19:16:58 +0300

    Initial commit

---
+++ FactorialDevision/Program.cs
@@ -0,0 +1,26 @@
+﻿class FactorialDivision
+{
+    static void Main()
+    {
+        
+        int number1 = int.Parse(Console.ReadLine());
+
+        
+        int number2 = int.Parse(Console.ReadLine());
+
+        int factorial1 = CalculateFactorial(number1);
+        int factorial2 = CalculateFactorial(number2);
+
+        int result = factorial1 / factorial2;
+
+        Console.WriteLine($"{result}");
+    }
+
+    static int CalculateFactorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * CalculateFactorial(n - 1);
+    }
+}
